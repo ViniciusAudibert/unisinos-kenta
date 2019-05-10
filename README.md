6ddf14cc849143be7b87eb56010c541431342911
Vinicius Audibert <vini.audibert@gmail.com>
2019-05-09 23:38:52 -0300

    feat: adicionar projeto

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# unisinos-kenta
+# Kenta
+Projeto criado para a aula `Arquitetura de Software` na Unisinos.
+
+### Tecnologias
+Foram utilizados as seguintes tecnologias nesse projeto
+* Spring
+* Lombok
+* JPA
+* Spring MVC
+* Tomcat
+* H2 Database
+* React
+
+### Iniciar aplicação
+// TODO
---
+++ src/main/java/com/br/kenta/KentaApplication.java
@@ -0,0 +1,13 @@
+package com.br.kenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KentaApplication.class, args);
+	}
+
+}
